5d0083898785d99448f228236f7842fed8d6b0cd
Thiago Santos <61941921+ThiagoSantosTh@users.noreply.github.com>
2021-04-15 14:02:50 -0300

    Update

--- js/script.js
+++ js/script.js
@@ -31,7 +31,7 @@ window.onload = digitar
 
 // Text que escreve a apaga aos poucos
 var div = document.getElementById('log');
-var textos = ['Programador JavaScript.' ,'UI & UX Design.','Desenvolvedor Front end.', 'Programador PHP.', 'Programador Web.', 'Trainee  React Js.', 'Trainee Node Js.', 'Web Design.'];
+var textos = ['Programador JavaScript.' ,'UI Design.','Desenvolvedor Front end.', 'Programador PHP.', 'Programador Web.', 'Trainee  React Js.', 'Trainee Node Js.', 'Web Design.'];
 
 function escrever(str, done) {
     var char = str.split('').reverse();
