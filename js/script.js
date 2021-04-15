d5428826b900a2e102a32bb8a1f55f3a8d9ec70f
Thiago Santos <61941921+ThiagoSantosTh@users.noreply.github.com>
2021-04-15 19:41:17 -0300

    jS

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ function digitar(){
     result = document.getElementById("result")
     window.setTimeout(function() {inserir(texto[count])}, 50)
 }
-function inserir(letra){ 
+function inserir(letra){  
     result.innerHTML += letra
     count++
     if(count < texto.length)
