30464f5527dd2e85fb4120728f66ab9cb2dca693
Thiago Santos <61941921+ThiagoSantosTh@users.noreply.github.com>
2021-08-06 11:08:01 -0300

    React JS

--- js/script.js
+++ js/script.js
@@ -31,7 +31,7 @@ window.onload = digitar
 
 // Text que escreve a apaga aos poucos
 var div = document.getElementById('log');
-var textos = ['Programador JavaScript.' ,'UI Design.','Desenvolvedor Front end.', 'Programador PHP.', 'Programador Web.', 'Trainee  React Js.', 'Trainee Node Js.', 'Web Design.'];
+var textos = ['Programador JavaScript.' ,'UI Design.','Desenvolvedor Front end React JS.', 'Programador Web.', 'Trainee  React Native.', 'Trainee Node Js.', 'Web Design.'];
 
 function escrever(str, done) {
     var char = str.split('').reverse();
